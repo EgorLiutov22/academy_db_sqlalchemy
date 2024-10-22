03064fcf688e9bd44f6ca89e37d3bf65ab37b946
lyutov_e <eliutov22@gmail.com>
2024-10-22 21:14:35 +0500

    add classes

---
+++ doctorandpatient.py
@@ -0,0 +1,8 @@
+class Doctor:
+    def __str__(self):
+        return "doctor"
+
+
+class Patient:
+    def __str__(self):
+        return "patient"
--- make_classes.py
+++ make_classes.py
@@ -1,12 +1,10 @@
-class Doctor:
-    pass
-
-
-class Patient:
-    pass
+import inspect
+import doctorandpatient
 
 def make_somebody(classname, **kwargs):
-    classes = {'doctor': Doctor, 'patient': Patient}
+    # classes = {'doctor': Doctor, 'patient': Patient}
+    all_members = inspect.getmembers(doctorandpatient)
+    classes = {str(m[0]).lower(): m[1] for m in all_members if inspect.isclass(m[1])}
     s = classes[classname]()
     return s
 
